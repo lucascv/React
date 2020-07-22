cc520153634a22e75689750436d9ca5dcfd173d7
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-22 00:07:45 -0300

    add aula 23

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -7,6 +7,7 @@ import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 export default () => (
   <div className="App">
@@ -14,9 +15,11 @@ export default () => (
 
     <div className="Cards">
       <Card titulo="#05 - Componente com filhos" color="#00C8F8">
-        <Familia
-          sobrenome="Ferreira"
-        />
+        <Familia sobrenome="Silva">
+          <FamiliaMembro nome="Pedro" />
+          <FamiliaMembro nome="Ana" />
+          <FamiliaMembro nome="Gustavo" />
+        </Familia>
       </Card>
 
       <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
