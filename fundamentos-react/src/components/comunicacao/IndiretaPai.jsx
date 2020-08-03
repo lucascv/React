714e45479705bb2f9d3c6007e04a4b808653731d
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-03 19:38:37 -0300

    add aula 32

--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 export default props => {
+  const [nome, setNome] = useState('?');
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
+  function fornecerInformacoes(nome, idade, nerd) {
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
+  }
+
   return (
     <div>
-      <div>Pai</div>
-      <IndiretaFilho />
+      <span>{nome} </span>
+      <span><strong>{idade} </strong></span>
+      <span> {nerd ? 'Verdadeiro' : 'Falso'}!</span>
+      <IndiretaFilho quandoClicar={fornecerInformacoes} />
     </div>
   );
 }
