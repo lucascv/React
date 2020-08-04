b70f5329af74ba5f282b9fd1cb56bb4499b60a6c
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-03 23:20:49 -0300

    add aula 33

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,12 +14,17 @@ import ParOuImpar from "./components/condicional/ParOuImpar";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
+import Input from "./components/formulario/Input";
 
 export default () => (
   <div className="App">
     <h1>Fundamentos React (Arrow)</h1>
 
     <div className="Cards">
+      <Card titulo="#11 - Componente Controlado" color="#E45F56">
+        <Input />
+      </Card>
+
       <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
         <IndiretaPai />
       </Card>
